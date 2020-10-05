d558bba4145eed62d617aaf26b3ac72a97510050
Robnoo02 <robbenooyens@hotmail.com>
2020-10-06 00:25:46 +0200

    Added hiddem voicechat

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { table } from "console";
-import { Client, GuildMember, Message, MessageReaction, PartialUser, ReactionEmoji, ReactionManager, Role, TextChannel, User } from "discord.js";
+import { Activity, ActivityOptions, Client, GuildMember, Message, MessageReaction, PartialUser, PresenceManager, ReactionEmoji, ReactionManager, Role, TextChannel, User, VoiceState } from "discord.js";
 import { BombMSG } from "./bombmsg";
 import { getIP } from "./iputils";
 
@@ -13,10 +13,15 @@ let a_role: Role;
 let b_role: Role;
 let c_role: Role;
 let d_role: Role;
+let voice_role: Role;
 
 
-client.on('ready', () => {
+client.on('ready', async () => {
   console.log(`Logged in as ${client.user.tag}!`);
+
+  const f: ActivityOptions = { type: "PLAYING", name: "lvk.sh/ictbot" };
+  const r = await client.user.setActivity(f);
+  console.log('Activity Set');
 
   client.guilds.cache.forEach(async (g) => {
 
@@ -26,6 +31,12 @@ client.on('ready', () => {
     b_role = g.roles.cache.filter((a) => { return a.id == '757968245474066523' }).first();
     c_role = g.roles.cache.filter((a) => { return a.id == '757968282841120788' }).first();
     d_role = g.roles.cache.filter((a) => { return a.id == '757968318685642833' }).first();
+    voice_role = g.roles.cache.filter((a) => { return a.id == '762799243521294358' }).first();
+
+    // g.roles.cache.forEach((r) => {
+    //   // TODO: DEBUG REMOVE
+    //   console.log(r.id + " " + r.name);
+    // });
 
     g.channels.cache.filter((a) => {
       return a instanceof TextChannel;
@@ -71,7 +82,7 @@ client.on('messageReactionAdd', async (a: MessageReaction, b: User | PartialUser
 
       let roles = [];
       for (let f of [a_role, b_role, c_role, d_role] as Role[]) {
-        if (u.roles.cache.some((a) => {return a.id == f.id})) {
+        if (u.roles.cache.some((a) => { return a.id == f.id })) {
           console.log('user has ' + f.name);
           roles.push(f);
         }
@@ -126,7 +137,7 @@ client.on('messageReactionRemove', async (a: MessageReaction, b: User | PartialU
 
       let roles = [];
       for (let f of [a_role, b_role, c_role, d_role] as Role[]) {
-        if (u.roles.cache.some((a) => {return a.id == f.id})) {
+        if (u.roles.cache.some((a) => { return a.id == f.id })) {
           console.log('user has ' + f.name);
           roles.push(f);
         }
@@ -141,7 +152,7 @@ client.on('messageReactionRemove', async (a: MessageReaction, b: User | PartialU
 
       for (let ark of Object.keys(map)) {
         if (a.emoji.name == ark) {
-          roles = roles.filter((afr: Role) => {return afr.id != map[ark].id});
+          roles = roles.filter((afr: Role) => { return afr.id != map[ark].id });
           u.roles.remove(map[ark]);
         }
       }
@@ -155,6 +166,15 @@ client.on('messageReactionRemove', async (a: MessageReaction, b: User | PartialU
   }
 });
 
+client.on('voiceStateUpdate', async (oldState, newState) => {
+  if (oldState.channel == null && newState.channel != null) {
+    newState.member.roles.add([voice_role]);
+  }
+  if (oldState.channel != null && newState.channel == null) {
+    newState.member.roles.remove(voice_role);
+  }
+});
+
 client.on('message', async (msg: Message) => {
   if (!(msg.channel instanceof TextChannel))
     return;
@@ -165,14 +185,18 @@ client.on('message', async (msg: Message) => {
     BombMSG.send({ channel: msg.channel, msg: 'List of Commands ```javascript\n!invite // Laat de invite link zien\n!ping // Ping!\n!ip // Laat uw IP-Address zien```Self Destructs in %s', alsoDelete: [msg], timeUntilDeletion: 20, timeUntilUpdate: 5 });
     return;
   }
+  if (msg.content === '!aot') {
+    BombMSG.send({ channel: msg.channel, msg: 'Shinzou wo Sasageyo```Self Destructs in %s', alsoDelete: [msg], timeUntilDeletion: 20, timeUntilUpdate: 5 });
+    return;
+  }
   if (msg.content === '!invite') {
     BombMSG.send({ channel: msg.channel, msg: 'INVITE LINK: https://lvk.sh/ict\n\nThis message self destructs in %s seconds!', timeUntilDeletion: 5, alsoDelete: [msg], timeUntilUpdate: 1 })
     // const m = await BombMSG.send(msg.channel, 'INVITE LINK: https://lvk.sh/ict\n\nThis message self destructs in %s seconds!', 5, [msg]);
     return;
   }
   if (msg.content === '!links') {
-    //BombMSG.send({ channel: msg.channel, msg: 'Facebook WINAK group: https://www.facebook.com/groups/787046402121728\nWINAK website: https://www.winak.be/\nDiscord invite link: https://lvk.sh/ict\nBlackboard: https://blackboard.uantwerpen.be/\nFree hacking tools: https://zws.im/‍‌‌‌‌‌‌‍‍‍ \n', timeUntilDeletion: 5, alsoDelete: [msg], timeUntilUpdate: 1 })
-    msg.channel.send('\nFacebook WINAK group: <https://www.facebook.com/groups/787046402121728\>\nWINAK website: <https://www.winak.be/>\nDiscord invite link: <https://lvk.sh/ict>\nBlackboard: <https://blackboard.uantwerpen.be/>\nFree hacking tools: <https://zws.im/‍‌‌‌‌‌‌‍‍‍> \n');
+    BombMSG.send({ channel: msg.channel, msg: '\nFacebook WINAK group: <https://www.facebook.com/groups/787046402121728\>\nWINAK website: <https://www.winak.be/>\nDiscord invite link: <https://lvk.sh/ict>\nBlackboard: <https://blackboard.uantwerpen.be/>\nCSA: <http://msdl.cs.mcgill.ca/people/hv/teaching/ComputerSystemsArchitecture/>\nGAS: <https://app.perusall.com/courses/gegevensabstractie-en-structuren/_/dashboard/startup>\nIP (Inginious): <https://inginious.uantwerpen.be/course/IP2021>', timeUntilDeletion: 60, alsoDelete: [msg], timeUntilUpdate: 1 })
+    // msg.channel.send('\nFacebook WINAK group: <https://www.facebook.com/groups/787046402121728\>\nWINAK website: <https://www.winak.be/>\nDiscord invite link: <https://lvk.sh/ict>\nBlackboard: <https://blackboard.uantwerpen.be/>\nFree hacking tools: <https://zws.im/‍‌‌‌‌‌‌‍‍‍> \n');
     return;
   }
   if (msg.content.startsWith('!ip')) {
