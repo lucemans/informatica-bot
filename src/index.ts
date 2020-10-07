5a16495e92efa63e8f12b18f95cd8688bef1b76d
Lucemans <luc@lucemans.nl>
2020-10-07 17:26:24 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 require('dotenv').config();
-
+-=-
 const Discord = require('discord.js');
 const client: Client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 
